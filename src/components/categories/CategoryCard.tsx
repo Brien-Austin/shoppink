2051e061bcbfb950ef4bab7fc8895c50bb791d0e
Brien-Austin <brienaustinclayton@gmail.com>
2024-01-02 14:22:14 +0530

    ui change

--- src/components/categories/CategoryCard.tsx
+++ src/components/categories/CategoryCard.tsx
@@ -11,7 +11,7 @@ const CategoryCard:React.FC<categoryProps> = ({id,name,img}) => {
   <div key={id} className='carousel relative'>
 
     <img src={img} className='object-cover brightness-50 rounded lg:w-72 lg:h-48 sm:h-24 sm:w-24' alt="" />
-    <h1 className='absolute inset-0  flex items-center justify-center text-white text-shadow lg:text-md sm:text-sm whitespace-nowrap'>{name}</h1>
+    <h1 className='absolute inset-0  flex items-center justify-center text-white text-shadow lg:text-md sm:text-[10px] whitespace-nowrap'>{name}</h1>
   </div>
 </>
 
