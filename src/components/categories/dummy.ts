001cba50147012ee7c800833120f00503a4d37a3
Brien-Austin <brienaustinclayton@gmail.com>
2024-01-03 04:32:45 +0530

    clothing

--- src/components/categories/dummy.ts
+++ src/components/categories/dummy.ts
@@ -7,7 +7,7 @@ type categories = {
 export const categoriesData:categories[] = [
     {
       id: 1,
-      name: 'Apparel',
+      name: 'Clothing',
       link: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbnxlbnwwfHwwfHx8MA%3D%3D',
     },
     {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import SmNavBar from "@/components/NavBar/SmNavBar";
 import NavBar from "../components/NavBar/LgNavBar";
 import Categories from "../components/categories/Categories";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
+/*import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";*/
 import React from "react";
 
 
@@ -10,21 +10,21 @@ import React from "react";
 
 
 const  Home:React.FC =()=>{
-    const select = useSelector((state:RootState)=>state.nav.selected)
+   /* const select = useSelector((state:RootState)=>state.nav.selected)*/
     return (
         <>
       
    
-     <h1 className="mx-16 sm:mx-5 mt-5 text-2xl  font-bold text-pink-600 sm:block lg:hidden">Shop<span className="text-slate-800">Pink</span></h1>
+     <h1 className="mx-16 sm:mx-5 mt-5 text-2xl  font-bold sm:block lg:hidden text-slate-800">Shop<span className="text-pink-600 ">Pink</span></h1>
     
  
         <div className="">
         <NavBar/>
         <SmNavBar/>
        <div className="mx-16 sm:mx-5">
-       <h1 className="mt-4 lg:text-2xl sm:text-xl font-bold text-slate-800">Explore Categories</h1>
+       <h1 className="mt-8 lg:text-xl sm:text-md font-[500] text-slate-800">Explore Categories</h1>
        <Categories/>
-       <h1>{select}</h1>
+       
       
        </div>
         </div>
