13a3133279a16f89a49a3ed9503c92b4756ea9b8
Brien-Austin <brienaustinclayton@gmail.com>
2024-01-02 08:55:35 +0530

    sm navbar ui changes

--- src/components/NavBar/SmNavBar.tsx
+++ src/components/NavBar/SmNavBar.tsx
@@ -6,11 +6,11 @@ const SmNavBar:React.FC = () => {
     <nav className=" lg:hidden sm:block fixed bottom-0 left-0 w-full border  text-white p-4 px-5">
     
     <div className='flex justify-center items-center gap-5 mx-5'>
-    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><Home className='text-slate-700'/>Home</div>
-    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><List className='text-slate-700'/>Categories</div>
-    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><Bell className='text-slate-700'/>Notifications</div>
-    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><User className='text-slate-700'/>Account</div>
-    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><ShoppingBag className='text-slate-700'/>Cart</div>
+    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><Home className='text-slate-700 text-[8px]'/>Home</div>
+    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><List className='text-slate-700 text-[8px]'/>Categories</div>
+    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><Bell className='text-slate-700 text-[8px]'/>Notifications</div>
+    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><User className='text-slate-700 text-[8px]'/>Account</div>
+    <div className='flex flex-col gap-2 items-center text-black text-[10px]'><ShoppingBag className='text-slate-700 text-[8px]'/>Cart</div>
    
     </div>
   </nav>
